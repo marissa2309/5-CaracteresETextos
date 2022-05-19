ff3ed6069047f0300b9f912c7bb75d5e42a6c421
marissa2309 <102029727+marissa2309@users.noreply.github.com>
2022-05-19 09:52:50 -0300

    Add files via upload

---
+++ Program_2.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 02 - Criando Variaveis");
+        int idade;
+
+        idade = 27;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 27 - 5;
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 5 * 2 - 6;
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = (5 - 2) * 3;
+        Console.WriteLine("Minha idade é " + idade);
+
+        Console.WriteLine("Precione ENTER para encerrar");
+        Console.ReadLine();
+
+    }
+}
---
+++ Program_4.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 04 - Conversões e Outros Tipos");
+
+        double salario;
+        salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+
+        //O long é um tipo de variavel de 64 bits 
+        long x;
+        x = 200000000;
+        Console.WriteLine(x);
+
+        short y;
+        y = 150;
+        Console.WriteLine(y);
+
+        float altura;
+        altura = 1.72f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine ("Pressione ENTER para encerrar ");
+        Console.ReadLine();
+    }
+}
